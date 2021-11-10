9f57c69feb13f4050cce2fe11c42e5aec99025ec
kimtaeho <>
2021-11-11 01:51:25 +0900

    총알 발사

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectilePrefab;
+    [SerializeField]
+    private float attackRate = 0.1f;
+
+    public void StartFiring()
+    {
+        StartCoroutine("TryAttack");
+    }
+
+    public void StopFiring()
+    {
+        StartCoroutine("TryAttack");
+    }
+
+    private IEnumerator TryAttack()
+    {
+        while(true)
+        {
+            Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(attackRate);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
