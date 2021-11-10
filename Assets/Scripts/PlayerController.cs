e446a6f86680f8cb868a1f89f45b2054d00eb08a
kimtaeho <>
2021-11-10 16:40:40 +0900

    첫 주인공 움직임

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Movement2D movement2D;
+    private void Awake()
+    {
+        movement2D = GetComponent<Movement2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        movement2D.MoveTo(new Vector3(x, y, 0));
+    }
+}
