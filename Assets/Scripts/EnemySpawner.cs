50b9b27befb167b4ea7bfec151bfcd331631b8e9
kimtaeho <>
2021-11-11 18:45:04 +0900

    운석처리까지

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private StageData stageData;
+    [SerializeField]
+    private float spawnTime;
+
+
+    private void Awake()
+    {
+        StartCoroutine("SpawnEnemy");
+
+    }
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            float positionX = Random.Range(stageData.LimitMin.x, stageData.LimitMax.x);
+            Instantiate(enemyPrefab, new Vector3(positionX,stageData.LimitMax.y + 1.0f, 0.0f), Quaternion.identity);
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Twinkle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Twinkle : MonoBehaviour
+{
+    private float fadeTime = 0.1f;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        StartCoroutine("TwinkleLoop");
+    }
+
+    private IEnumerator TwinkleLoop()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(FadeEffect(0, 1));
+            yield return StartCoroutine(FadeEffect(1, 0));
+        }
+    }
+
+    private IEnumerator FadeEffect(float start, float end)
+    {
+        float currentTime = 0.0f;
+        float percent = 0.0f;
+
+        while(percent < 1)
+        {
+            currentTime = Time.deltaTime;
+            percent = currentTime / fadeTime;
+
+            Color color = spriteRenderer.color;
+            color.a = Mathf.Lerp(start, end, percent);
+            spriteRenderer.color = color;
+
+
+            yield return null;
+        }
+
+    }
+}
